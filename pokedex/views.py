b180a8855457e9ecb91b1915b0f4aeb770087144
Ajdiazs <ajdiazs@puce.edu.ec>
2024-07-06 12:51:17 -0500

    laboratorio 8

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Pokemon
 
 def index(request):
-    pokemons = ['charmander', 'pikachu', 'squirtle']
+    ##pokemons = Pokemon.objects.all() ##Select * from pokedex_pokemon
+    pokemons = Pokemon.objects.order_by('name')##Select * from pokedex_pokemon order by name
+
     template = loader.get_template('index.html')
     return HttpResponse(template.render({'pokemons': pokemons}, request))
 
-def pokemon(request, pokemon):
+def pokemon(request, pokemon_id):
+    ##SELECT * FROM POKEDEX_POKEMON WHERE id=pokemon_id
+    pokemon = Pokemon.objects.get(id=pokemon_id)
     template = loader.get_template('display_pokemon.html')
     context = {
-        'pokemon': pokemon
-    }
+        'pokemon': pokemon   
+        }
     return HttpResponse(template.render(context, request))
