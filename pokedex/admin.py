a1f2b8fd92254b42f9bfce4a74dcfdc31e1a2f96
ajdiaz105 <ajdiaz105@gmail.com>
2024-07-20 13:36:38 -0500

    Add files via upload

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon,Trainer
 
 # Register your models here.
 @admin.register(Pokemon)
 class Pokemonadmin(admin.ModelAdmin):
     pass
+
+@admin.register(Trainer)
+class Traineradmin(admin.ModelAdmin):
+    pass
+
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect, render
+from pokedex.forms import PokemonForm
 from .models import Pokemon
+
+
 
 def index(request):
     ##pokemons = Pokemon.objects.all() ##Select * from pokedex_pokemon
@@ -17,3 +21,12 @@ def pokemon(request, pokemon_id):
         'pokemon': pokemon   
         }
     return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method=='POST':
+        form = PokemonForm(request.POST, request.FILES)
+        form.save()
+        return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    return render(request, 'add_pokemon.html',{'form': form})
