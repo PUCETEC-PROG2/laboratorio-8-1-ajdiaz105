2b0fae57303dd9547ee9e57b95de0670122dff87
ajdiaz105 <ajdiaz105@gmail.com>
2024-07-25 18:33:17 -0500

    Login

--- pokedex/views.py
+++ pokedex/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import redirect, render
 from pokedex.forms import PokemonForm
 from .models import Pokemon
 
-
+#importación de autenticacion de usuario
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     ##pokemons = Pokemon.objects.all() ##Select * from pokedex_pokemon
@@ -22,6 +24,7 @@ def pokemon(request, pokemon_id):
         }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def add_pokemon(request):
     if request.method=='POST':
         form = PokemonForm(request.POST, request.FILES)
@@ -30,3 +33,6 @@ def add_pokemon(request):
     else:
         form = PokemonForm()
     return render(request, 'add_pokemon.html',{'form': form})
+
+class CustomLoginView(LoginView):
+    template_name="login.html"
